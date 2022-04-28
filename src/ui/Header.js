dbef894e08dc381e62fd55c168802b126fe84b91
avi9360 <savinash406@gmail.com>
2022-04-28 19:42:09 +0530

    Added new page animations

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -2,13 +2,31 @@ import config from "../../blog.config"
 import { MDXProvider } from "@mdx-js/react"
 import { Box, Heading } from "theme-ui"
 import Nav from "./Nav"
-
+import { motion } from "framer-motion"
 const Header = (props) => (
   <MDXProvider>
     <Box as="header" sx={{ textAlign: "center", pt: 5 }}>
-      <Heading sx={{ fontSize: 6, pb: 2, px: 3 }} as="h1">
-        {config.title}
-      </Heading>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.5,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 0.6,
+            },
+          },
+        }}
+      >
+        <Heading sx={{ fontSize: 6, pb: 2, px: 3 }} as="h1">
+          {config.title}
+        </Heading>
+      </motion.div>
       <Nav />
     </Box>
   </MDXProvider>
--- src/ui/Nav.js
+++ src/ui/Nav.js
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router"
 import { Box } from "theme-ui"
 import NavLink from "./NavLink"
-
 const Nav = (props) => {
   const router = useRouter()
   return (
